df6d98b864528479a78d00a7f72e588cdf8dc8a4
Игорь Потапов <igor@potapoff.org>
2022-08-23 15:07:45 +0300

    Cowboy loops

--- lib/middlewares.ex
+++ lib/middlewares.ex
@@ -54,6 +54,36 @@ defmodule Onion.Middlewares do
 				end
  			end
 
+			defmacro defloop name, opts \\ [], code do
+				quote do
+					defmodule unquote(name) do
+						alias Onion.Args, as: Args
+
+						defp break(args = %Args{middlewares: {a,b}}), do: %{ args | middlewares: {[], b} }
+						defp break!(args = %Args{middlewares: {a,b}}), do: %{ args | middlewares: {[], []} }
+
+						defp reply(args = %Args{ response: resp }, code), do: %{args | response: %{resp | code: code} } |> send
+						defp reply(body, args = %Args{ response: resp }, code), do: reply(args, code, body)
+						defp reply(args = %Args{ response: resp }, code, body), do: %{args | response: %{resp | code: code, body: body} } |> send
+						defp reply(args = %Args{ response: resp }, code, body, []), do: %{args | response: %{resp | code: code, body: body} } |> send
+						defp reply(args = %Args{ response: resp }, code, body, headers=[{_k,_v}|_t]), do: %{args | response: %{resp | code: code, body: body, headers: headers} } |> send
+						defp reply(body, args = %Args{ response: resp }, code, headers), do: reply(args, code, body, headers)
+
+						defp patch(args = %Args{context: context}, val) when is_map(val), do: %{args | context: Dict.merge(context, val)}
+						defp patch(args = %Args{context: context}, key, val) when is_atom(key), do: %{args | context: Dict.put(context, key, val)}
+						defp patch(args = %Args{context: context}, key, val) when is_list(key), do: %{args | context: put_in(context, key, val)}
+
+						defp send(args = %Args{ request: %{extra: %{pid: pid}} }), do: send(pid, {:done, args})
+						defp chunk(args = %Args{ request: %{extra: %{pid: pid}}, response: resp }, body), do: send(pid, {:chunk, %{args | response: %{resp | body: body} }})
+
+						unquote(code)
+
+						def init(args), do: {unquote(name), args}
+						def init, do: unquote(name)
+
+					end
+				end
+			end
  			
 		end
 	end
--- lib/routes.ex
+++ lib/routes.ex
@@ -87,6 +87,88 @@ defmodule Onion.Routes do
 				end
 			end #defmacro
 
+            defmacro loop path, opts do
+                name = Dict.get(opts, :name, :"#{U.uuid}")
+                timeout = Dict.get(opts, :timeout, 5000)
+                chunked = Dict.get(opts, :chunked, false)
+                chunked_headers = Dict.get(opts, :chunked_headers, [])
+                quote do
+
+                    routes = [{unquote(path), unquote(name), Enum.into(unquote(opts), %{})} | routes]
+                    defmodule unquote(name) do
+                        alias Onion.Args, as: Args
+                        require Logger
+
+                        def init({:tcp, :http}, req, args) do
+                            pid = elem(req, 4)
+                            {module, opts} = case Dict.get(args, :loop, nil) do
+                                {module, opts} -> {module, opts}
+                                module -> {module, nil}
+                            end
+                            state = %Args{request: %{extra: Dict.put(args, :pid, pid)}, cowboy: req,
+                                middlewares: {Dict.get(args, :middlewares, []),[]}} 
+                                |> process_in
+                            {:ok, req} = case unquote(chunked) do
+                                true -> :cowboy_req.chunked_reply(200, unquote(chunked_headers), req)
+                                _ -> {:ok, req}
+                            end
+                            state = Keyword.has_key?(module.__info__(:functions), :begin) && apply(module, :begin, [state, opts]) || state
+                            task = spawn fn -> apply(module, :loop, [state, opts]) end
+                            state = put_in(state, [:response, :extra, :pid], task)
+                                |> put_in([:response, :extra, :module], module)
+                            {:loop, req, state, unquote(timeout), :hibernate}
+                        end
+
+                        def info({:done, state}, req, _) when unquote(chunked) == false do
+                            %Args{response: %{code: code, headers: headers, body: body}} = process_out(state)
+                            {:ok, req} = :cowboy_req.reply(code, headers, body, req)
+                            {:ok, req, state}
+                        end
+                        def info({:done, _}, req, state), do: {:ok, req, state}
+                        def info({:chunk, _state}, req, state) do
+                            %Args{response: %{body: body}} = process_out(_state)
+                            :ok = :cowboy_req.chunk(body, req)
+                            {:loop, req, state, :hibernate}
+                        end
+
+                        defp take_request_middleware(args=%Args{ middlewares: {[],_} }), do: {:empty, args}
+                        defp take_request_middleware(args=%Args{ middlewares: {[head|tail], m} }), do: {:ok, head, %{args | middlewares: {tail, [head|m]}}}
+
+                        defp process_in(args) do
+                            case take_request_middleware(args) do
+                                {:ok, middleware, args} ->
+                                    args = case middleware do
+                                        {middleware, opts} -> process_in(apply(middleware, :process, [:in, args, opts]))
+                                        middleware ->         process_in(apply(middleware, :process, [:in, args, []]  ))
+                                    end
+                                {:empty, args} -> args
+                            end
+                        end
+                        defp process_out(args=%Args{ middlewares: {_, []} }), do: args
+                        defp process_out(args=%Args{ middlewares: {inm, [middleware|tail]} }) do
+                            args = %{ args | middlewares: {inm, tail} }
+                            args = case middleware do
+                                {middleware, opts} -> apply(middleware, :process, [:out, args, opts])
+                                middleware -> apply(middleware, :process, [:out, args, []])
+                            end
+                            process_out(args)
+                        end
+
+                        def terminate(_, _, state = %{response: %{extra: %{pid: task, module: module}}}) do 
+                            if Keyword.has_key?(module.__info__(:functions), :done), do: 
+                                apply(module, :done, [state])
+                            Process.exit(task, :kill)
+                            :ok
+                        end
+                        def terminate(_, _, state = %{response: %{extra: %{module: module}}}) do 
+                            if Keyword.has_key?(module.__info__(:functions), :done), do: 
+                                apply(module, :done, [state])
+                            :ok
+                        end
+                        def terminate(_, _, _), do: :ok
+                    end
+                end
+            end #defmacro
 
             defmacro macro_get_routes(routes) do
                 quote unquote: false do
