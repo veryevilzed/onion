de48932e952f4d079a29ec4d5ad63343548b3a88
Igor Potapov <skazi@Igors-MacBook-Pro.local>
2025-10-29 14:53:12 +0300

    Fix old deps

--- mix.exs
+++ mix.exs
@@ -17,7 +17,9 @@ defmodule Onion.Mixfile do
 
   defp deps do
     [
-      { :cowboy, git: "https://github.com/extend/cowboy", branch: "1.0.x" }
+      { :cowboy, git: "https://github.com/extend/cowboy", tag: "fd37fad592fc96a384bcd060696194f5fe074f6f" },
+      { :cowlib, git: "https://github.com/ninenines/cowlib", tag: "d544a494af4dbc810fc9c15eaf5cc050cced1501", override: true },
+      { :ranch, git: "https://github.com/ninenines/ranch", tag: "adf1822defc2b7cfdc7aca112adabfa1d614043c", override: true }
     ]
   end
 end
